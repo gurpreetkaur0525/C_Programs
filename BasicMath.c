5cf3101fc61cac8f020152953ba9abf962207d8c
gurpreet kaur <gurpreetkaurgill@gurpreets-MacBook-Air.local>
2020-11-27 15:36:53 +0530

    inital commit for helloworld and basicmath programs

---
+++ BasicMath.c
@@ -0,0 +1,16 @@
+//
+//  BasicMath.c
+//  
+//
+//  Created by gurpreet kaur on 11/27/20.
+//
+
+#include <stdio.h>
+int main()
+{
+    int a=0,b=1,c=5,d=3,e=4;
+    a=b-c+d*e;
+    printf("\n %d", a);
+    return 0;
+}
+
---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.c
+//
+//  Created by gurpreet kaur on 11/27/20.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World, Gurpeet!\n");
+    return 0;
+}
